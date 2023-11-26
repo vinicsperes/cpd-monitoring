81a54bbcf293dc6f5edff6f7a9aca0911643938f
vinicsperes <peresvini2@gmail.com>
2023-11-25 22:16:06 -0300

    Create endpoint to show data

--- src/server.js
+++ src/server.js
@@ -2,19 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const server = express();
-
 server.use(bodyParser.json());
+
+let ultimoDado = null;
 
 server.post('/dados', (req, res) => {
   const { temperatura, umidade, gas } = req.body;
   console.log('Dados recebidos:', { temperatura, umidade, gas });
+
+  // Armazene o último dado
+  ultimoDado = { temperatura, umidade, gas };
+
   res.status(200).send('Dados recebidos com sucesso.');
 });
 
-server.get('/mensagem', (req, res) => {
-    res.status(200).send('Esta é uma mensagem para a rota GET.');
-  });
-  
+server.get('/ultimodado', (req, res) => {
+  if (ultimoDado) {
+    res.status(200).json(ultimoDado);
+  } else {
+    res.status(404).send('Nenhum dado encontrado.');
+  }
+});
+
 server.listen(3000, () => {
-  console.log(`Servidor rodando`);
+  console.log('Servidor rodando na porta 3000');
 });
